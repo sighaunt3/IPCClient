ddf28eb0d1496b81a0a4963f41bbe36a1cd87e0a
sighaunt3 <140482931+sighaunt3@users.noreply.github.com>
2023-10-12 09:49:19 +0300

    Update MessengerService.kt

--- app/src/main/java/com/example/albumnextbutton/MessengerService.kt
+++ app/src/main/java/com/example/albumnextbutton/MessengerService.kt
@@ -42,38 +42,27 @@ class MessengerService : Service(){
         get() = (application as Helper).serverprop
 
     private var serverMessenger: Messenger? = null
-
-    // Messenger on the client
     private var clientMessenger: Messenger? = null
-
     private var infoUs = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
-
-    // Service Connection
     private val serviceConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            // Called when the connection to the server service is established
             serverMessenger = Messenger(service)
             clientMessenger = Messenger(handler)
             sendMessageToServer()
-            Log.d("Messenger", "Send Message")
+           
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
-            // Called when the connection to the server service is disconnected
             serverMessenger = null
             clientMessenger = null
         }
     }
 
-    // Handle messages from the remote service (server app)
     var handler: Handler = object : Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message) {
-            // Update UI with remote process info
-
             val bundle = msg.data
             val serverClass = Serverprop(bundle.getInt(PID).toString(), bundle.getInt(
                 CONNECTION_COUNT).toString())
-
             serverprop.serverData.postValue(serverClass)
         }
     }
@@ -81,53 +70,27 @@ class MessengerService : Service(){
     val apiHandler = Handler()
     private val apiRunnable = object : Runnable {
         override fun run() {
-            // Make the API call and update MutableLiveData with the response data
             GlobalScope.launch{
-                Log.d("Messenger", "Fetching data from API")
                 sharedViewModel.sharedMutableData.postValue(infoUs.getFact().body())
-                    Log.d("Messenger", "Sending data to Server App")
                     sendMessageToServer()
 
             }
-            // Make the call every minute
             apiHandler.postDelayed(this, 10000)
         }
     }
 
-    // apiHandler.removeCallbacks(apiRunnable)
-
     private val dataObserver = Observer<Boolean> {
         if(it){
-            try {
-                doBindService()
-            }
-            catch (e : Exception){
-                Log.w("MessengerError", e.toString())
-                val LaunchIntent = packageManager.getLaunchIntentForPackage("com.example.messengerserverapplication")
-                runBlocking {
-                    startActivity(LaunchIntent)
-                    delay(2000)
-                }
-                Log.w("Messenger", "Trying to wake up Server App")
-                try {
-                    doBindService()
-                }catch (e : Exception){
-                    Log.w("MessengerError", e.toString())
-                }
-            }
+            doBindService()
+         
         }
         else{
-            try {
-                doUnbindService()
-            }catch (e : Exception){
-                // First Start
-            }
-
+            doUnbindService()
+            
         }
     }
 
     override fun onBind(p0: Intent?): IBinder? {
-        // ?
         return null
     }
 
@@ -141,23 +104,18 @@ class MessengerService : Service(){
                 description = descriptionText
             }
 
-            // Register the channel with the system
             println("here")
             doBindService()
             val notificationManager = getSystemService(NotificationManager::class.java)
             notificationManager.createNotificationChannel(channel)
         }
-
-        // Start the service as a foreground service with a notification
         val notification = createNotification() // Implement the createNotification() method
         startForeground(1, notification)
 
         super.onCreate()
     }
     private fun createNotification(): Notification {
-        // Create and return the notification for the foreground service
-        // You can customize the notification as needed
-        // For example:
+    
         val notificationTitle = "Messenger Service"
         val notificationText = "Service is running in the background"
         val pendingIntent = PendingIntent.getActivity(
@@ -183,18 +141,12 @@ class MessengerService : Service(){
         apiHandler.removeCallbacks(apiRunnable)
         sharedButtonListener.sharedButton.removeObserver(dataObserver)
         doUnbindService()
-        // Stop the foreground service and remove the notification
         stopForeground(true)
         super.onDestroy()
     }
 
-    // Start service according to the button
     private fun doBindService(){
         println("hello22")
-        Log.d("atakan", "ALREADY connected")
-
-
-        // Start the server service
         Intent("messengerexample").also { intent ->
             intent.`package` = "com.example.messengerserverapplication"
             startService(intent)
@@ -202,44 +154,29 @@ class MessengerService : Service(){
         }
         println("hello22")
 
-        Log.d("Messenger", "SUCCESSFULLY connected")
     }
 
-    // Stop service activity according to the button activity
     private fun doUnbindService(){
         if(sharedButtonListener.sharedButton.value == null){
             try {
                 unbindService(serviceConnection)
-                Log.d("Messenger", "SUCCESSFULLY disconnected")
 
             }catch (e : Exception)
             {
-                Log.w("Messenger", e.toString())
             }
         }
         if(sharedButtonListener.sharedButton.value!!){
-            Log.d("Messenger", "ALREADY disconnected")
             return
         }
-        try {
-            unbindService(serviceConnection)
-            Log.d("Messenger", "SUCCESSFULLY disconnected")
-
-        }catch (e : Exception)
-        {
-            Log.w("Messenger", e.toString())
-        }
+        unbindService(serviceConnection)
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        // Start the periodic API calls and send data if the service is bounded
         apiHandler.post(apiRunnable)
-
         return START_STICKY
     }
 
     override fun onTaskRemoved(rootIntent: Intent) {
-        Log.d("atakan", "Task Removed, Restarting Service")
         apiHandler.removeCallbacks(apiRunnable)
         val restartServiceIntent = Intent(applicationContext, this.javaClass)
         restartServiceIntent.setPackage(packageName)
@@ -249,11 +186,6 @@ class MessengerService : Service(){
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun sendMessageToServer(){
-        if (serverMessenger == null) {
-            // Server service connection is lost or not available
-            Log.w("Messenger Exception", "Server service connection lost. Cannot send message.")
-            return
-        }
         println("send messassa")
         val message = Message.obtain(handler)
         val bundle = Bundle()
@@ -267,15 +199,10 @@ class MessengerService : Service(){
         bundle.putString(DATA, currData.toString())
         bundle.putString(ZAMAN,formatted.toString())
         message.data = bundle
-        message.replyTo = clientMessenger //for communication to be two-way
+        message.replyTo = clientMessenger 
+        serverMessenger?.send(message)
+         message.recycle()
 
-        try {
-            serverMessenger?.send(message)
-        } catch (e: RemoteException) {
-            e.printStackTrace()
-        } finally {
-            message.recycle()
-        }
     }
 
 }
